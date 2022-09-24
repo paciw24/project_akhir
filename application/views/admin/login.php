88ac329709ac9e0610253ce0fc8749fe4f97e552
farhanrafahadi <farhanrafahadi.xrpl2.2017@gmail.com>
2022-09-24 10:52:41 +0700

    Membuat logo

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -15,26 +15,26 @@
 <body>
     <div class="bg-default">
         <div class="container d-flex justify-content-center align-items-center">
-                <div class="card p-3 shadow">
-                    <form action="<?= base_url('prosesLogin') ?>" method="POST">
-                        <div class="mb-3">
-                            <h3 class="text-center text-uppercase">Berkah Laundry</h3>
-                        </div>
-                        <div class="mb-3">
-                            <label for="username">Username</label>
-                            <input type="text" class="form-control mt-1" placeholder="Masukkan Username">
-                        </div>
-                        <div class="mb-3">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control mt-1" placeholder="Masukkan Password">
-                        </div>
-                        <div class="mb-3">
-                            <button class="form-control">Login</button>
-                        </div>
-                    </form>
-                </div>
+            <div class="card p-3 shadow">
+                <form action="<?= base_url('prosesLogin') ?>" method="POST">
+                    <div class="mb-3">
+                        <h3 class="text-center text-uppercase">Berkah Laundry</h3>
+                    </div>
+                    <div class="mb-3">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control mt-1" placeholder="Masukkan Username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control mt-1" placeholder="Masukkan Password">
+                    </div>
+                    <div class="mb-3">
+                        <button class="form-control btn btn-secondary1 shadow">LOGIN</button>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
     </div>
     <!-- JAVASCRIPT -->
     <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
