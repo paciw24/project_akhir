0109e53298727cbfd915e799088e8452ec8066fd
farhanrafahadi <farhanrafahadi.xrpl2.2017@gmail.com>
2022-10-29 14:21:40 +0700

    database

--- db_laundry.sql
+++ db_laundry.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Sep 2022 pada 11.20
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 7.4.26
+-- Generation Time: Oct 29, 2022 at 09:20 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_detail_transaksi`
+-- Table structure for table `tb_detail_transaksi`
 --
 
 CREATE TABLE `tb_detail_transaksi` (
@@ -38,7 +38,7 @@ CREATE TABLE `tb_detail_transaksi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_member`
+-- Table structure for table `tb_member`
 --
 
 CREATE TABLE `tb_member` (
@@ -51,28 +51,21 @@ CREATE TABLE `tb_member` (
   `telp` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Struktur dari tabel `tb_outlet`
+-- Dumping data for table `tb_member`
 --
 
-CREATE TABLE `tb_outlet` (
-  `id` int(5) NOT NULL,
-  `nama` varchar(60) NOT NULL,
-  `alamat` text NOT NULL,
-  `telp` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `tb_member` (`id`, `nama`, `username`, `password`, `alamat`, `jenis_kelamin`, `telp`) VALUES
+(1, 'test', 'ok', 'ok', 'ok', 'L', '082190004060');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_paket`
+-- Table structure for table `tb_paket`
 --
 
 CREATE TABLE `tb_paket` (
   `id` int(5) NOT NULL,
-  `id_outlet` int(5) NOT NULL,
   `jenis` enum('kiloan','selimut','bed_cover','kaos','lain') NOT NULL,
   `nama_paket` varchar(60) NOT NULL,
   `harga` int(11) NOT NULL
@@ -81,29 +74,34 @@ CREATE TABLE `tb_paket` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_transaksi`
+-- Table structure for table `tb_transaksi`
 --
 
 CREATE TABLE `tb_transaksi` (
   `id` int(5) NOT NULL,
-  `id_outlet` int(5) NOT NULL,
   `kode_invoice` varchar(60) NOT NULL,
   `id_member` int(5) NOT NULL,
   `tgl` datetime NOT NULL,
-  `batas_waktu` datetime NOT NULL,
-  `tgl_bayar` datetime NOT NULL,
-  `biaya_tambahan` int(11) NOT NULL,
-  `diskon` double NOT NULL,
-  `pajak` int(11) NOT NULL,
   `status` enum('baru','proses','selesai','diambil') NOT NULL,
   `dibayar` enum('dibayar','belum_dibayar') NOT NULL,
+  `gambar` text NOT NULL,
   `id_user` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_transaksi`
+--
+
+INSERT INTO `tb_transaksi` (`id`, `kode_invoice`, `id_member`, `tgl`, `status`, `dibayar`, `gambar`, `id_user`) VALUES
+(1, 'P001', 1, '2022-10-29 07:48:06', 'selesai', 'belum_dibayar', 'test', 1),
+(2, 'P002', 1, '2022-10-29 08:08:10', 'baru', 'belum_dibayar', 'test', 1),
+(3, 'P003', 1, '2022-10-29 08:08:35', 'proses', 'dibayar', 'test', 1),
+(4, 'P004', 1, '2022-10-29 08:08:35', 'diambil', 'dibayar', 'test', 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_user`
+-- Table structure for table `tb_user`
 --
 
 CREATE TABLE `tb_user` (
@@ -111,16 +109,22 @@ CREATE TABLE `tb_user` (
   `nama` varchar(60) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` text NOT NULL,
-  `id_outlet` int(11) NOT NULL,
   `role` enum('admin','kasir','owner') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `role`) VALUES
+(1, 'Farhan', 'admin', 'admin', 'admin');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `tb_detail_transaksi`
+-- Indexes for table `tb_detail_transaksi`
 --
 ALTER TABLE `tb_detail_transaksi`
   ADD PRIMARY KEY (`id`),
@@ -128,103 +132,82 @@ ALTER TABLE `tb_detail_transaksi`
   ADD KEY `id_transaksi` (`id_transaksi`);
 
 --
--- Indeks untuk tabel `tb_member`
+-- Indexes for table `tb_member`
 --
 ALTER TABLE `tb_member`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `tb_outlet`
---
-ALTER TABLE `tb_outlet`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `tb_paket`
+-- Indexes for table `tb_paket`
 --
 ALTER TABLE `tb_paket`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `tb_transaksi`
+-- Indexes for table `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_member` (`id_member`),
-  ADD KEY `id_outlet` (`id_outlet`),
   ADD KEY `id_user` (`id_user`);
 
 --
--- Indeks untuk tabel `tb_user`
+-- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_outlet` (`id_outlet`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `tb_detail_transaksi`
+-- AUTO_INCREMENT for table `tb_detail_transaksi`
 --
 ALTER TABLE `tb_detail_transaksi`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_member`
+-- AUTO_INCREMENT for table `tb_member`
 --
 ALTER TABLE `tb_member`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_outlet`
---
-ALTER TABLE `tb_outlet`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `tb_paket`
+-- AUTO_INCREMENT for table `tb_paket`
 --
 ALTER TABLE `tb_paket`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_transaksi`
+-- AUTO_INCREMENT for table `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_user`
+-- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `tb_detail_transaksi`
+-- Constraints for table `tb_detail_transaksi`
 --
 ALTER TABLE `tb_detail_transaksi`
   ADD CONSTRAINT `tb_detail_transaksi_ibfk_1` FOREIGN KEY (`id_paket`) REFERENCES `tb_paket` (`id`),
   ADD CONSTRAINT `tb_detail_transaksi_ibfk_2` FOREIGN KEY (`id_transaksi`) REFERENCES `tb_transaksi` (`id`);
 
 --
--- Ketidakleluasaan untuk tabel `tb_transaksi`
+-- Constraints for table `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
   ADD CONSTRAINT `tb_transaksi_ibfk_1` FOREIGN KEY (`id_member`) REFERENCES `tb_member` (`id`),
-  ADD CONSTRAINT `tb_transaksi_ibfk_2` FOREIGN KEY (`id_outlet`) REFERENCES `tb_outlet` (`id`),
   ADD CONSTRAINT `tb_transaksi_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id`);
-
---
--- Ketidakleluasaan untuk tabel `tb_user`
---
-ALTER TABLE `tb_user`
-  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`id_outlet`) REFERENCES `tb_outlet` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
