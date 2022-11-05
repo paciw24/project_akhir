3ee9e85a98bd48a373c14f30dd1eb9a8c7ffcf74
Aryasomaw <agusdata97@gmail.com>
2022-11-05 11:42:20 +0700

    Membuat menu paket

--- application/views/admin/paket.php
+++ application/views/admin/paket.php
@@ -142,15 +142,17 @@
 
         function HapusMenu(id) {
             Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
+                title: 'yakin lu?',
+                text: "Ga bisa ulang lho!",
+                icon: 'Warnig',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
                 confirmButtonText: 'Yes, delete it!'
             }).then((result) => {
-                window.location.href = "<?= base_url('admin/paket/hapusPaket')?>/"+id;
+                if (result.isConfirmed) {
+                    window.location.href = "<?= base_url('admin/paket/hapusPaket')?>/"+id;
+                }
             })
         }
     </script>
